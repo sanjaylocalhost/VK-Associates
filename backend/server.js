9c5457eb3979d952b89d82f9c0f9f7a96cc367e1
Sanjay N <sanjayhot39@gmail.com>
2026-08-06 16:26:32 +0530

    cors fixed

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000', 'http://localhost:3001',"https://vk-associates-j9v1-seven.vercel.app",],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
